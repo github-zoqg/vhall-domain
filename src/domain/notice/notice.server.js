fdf0fb99e032c7dfffdcaca8ad347cd22d17eebf
chencheng.zeng <chencheng.zeng@vhall.com>
2021-10-22 15:02:49 +0800

    add(notice)：公告服务初始化

---
+++ src/domain/notice/notice.server.js
@@ -0,0 +1,34 @@
+export default function useNoticeServer(){
+    const state = {
+        //公告列表
+        contentList:[]
+    };
+
+    const init = (params={})=>{
+        const {roomId='',channelId=''} = params;
+        state.roomId = roomId;
+        state.channelId = channelId;
+    }
+
+    //更新当前公告列表
+    const updateContentList = (msg)=>{
+            state.contentList.unshift({
+                text: msg.room_announcement_text,
+                time: msg.push_time
+            });
+    }
+
+    //发送消息
+    const sendNotice = (params={})=>{
+            //todo 借助api发送消息,返回一个promise
+            const {content='',roomId='',channelId=''} = params;
+            let requestParams = {
+                room_id: state.roomId,
+                channel_id: state.channelId,
+                body: content
+            };
+            return Promise.resolve();
+    }
+
+    return {state,updateContentList,sendNotice}
+}
