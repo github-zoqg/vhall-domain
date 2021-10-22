930ed3e19b177121fda608487629bfdf3ece005f
chencheng.zeng <chencheng.zeng@vhall.com>
2021-10-22 15:47:26 +0800

    update(project)：notice服务代码格式化调整；默认的index入口文件导出的api增加noticeServer

--- src/domain/notice/notice.server.js
+++ src/domain/notice/notice.server.js
@@ -1,34 +1,34 @@
-export default function useNoticeServer(){
+export default function useNoticeServer() {
     const state = {
         //公告列表
-        contentList:[]
+        contentList: []
     };
 
-    const init = (params={})=>{
-        const {roomId='',channelId=''} = params;
+    const init = (params = {}) => {
+        const {roomId = '', channelId = ''} = params;
         state.roomId = roomId;
         state.channelId = channelId;
     }
 
     //更新当前公告列表
-    const updateContentList = (msg)=>{
-            state.contentList.unshift({
-                text: msg.room_announcement_text,
-                time: msg.push_time
-            });
+    const updateContentList = (msg) => {
+        state.contentList.unshift({
+            text: msg.room_announcement_text,
+            time: msg.push_time
+        });
     }
 
     //发送消息
-    const sendNotice = (params={})=>{
-            //todo 借助api发送消息,返回一个promise
-            const {content='',roomId='',channelId=''} = params;
-            let requestParams = {
-                room_id: state.roomId,
-                channel_id: state.channelId,
-                body: content
-            };
-            return Promise.resolve();
+    const sendNotice = (params = {}) => {
+        //todo 借助api发送消息,返回一个promise
+        const {content = '', roomId = '', channelId = ''} = params;
+        let requestParams = {
+            room_id: state.roomId,
+            channel_id: state.channelId,
+            body: content
+        };
+        return Promise.resolve();
     }
 
-    return {state,updateContentList,sendNotice}
+    return {state, updateContentList, sendNotice};
 }
