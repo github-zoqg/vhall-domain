615c33e6f9856055148ef018eadf4974df2deaf5
chencheng.zeng <chencheng.zeng@vhall.com>
2021-10-22 16:17:09 +0800

    update(project)：notice服务代码增加获取历史公告的api

--- src/domain/notice/notice.server.js
+++ src/domain/notice/notice.server.js
@@ -1,7 +1,18 @@
 export default function useNoticeServer() {
     const state = {
         //公告列表
-        contentList: []
+        noticeList: [],
+
+        //请求的分页参数
+        pageInfo:{
+            pos: 0,
+            limit: 10,
+            pageNum: 1
+        },
+        //总页数
+        totalPages:1,
+        //总条数
+        total:0,
     };
 
     const init = (params = {}) => {
@@ -12,9 +23,39 @@ export default function useNoticeServer() {
 
     //更新当前公告列表
     const updateContentList = (msg) => {
-        state.contentList.unshift({
+        state.noticeList.unshift({
             text: msg.room_announcement_text,
             time: msg.push_time
+        });
+    }
+
+    //获取消息记录
+    const getNoticeList = ({isFirst=false,roomId='',isCache=1})=>{
+
+        if (isFirst) {
+            state.noticeList = [];
+            state.pageInfo = {
+                pos: 0,
+                limit: 10,
+                pageNum: 1
+            };
+        }
+
+        //todo 待替换为相应服务
+       return  this.$vhallapi.notice.getNoticeHistoryList({
+            roomId: roomId,
+            is_cache: 1,
+            ...state.pageInfo
+        }).then(res => {
+            if (res.code == 200 && res.data) {
+                state.total = res.data.total;
+                if (!isFirst) {
+                    state.noticeList.push(...res.data.list)
+                } else {
+                    state.noticeList = res.data.list
+                }
+                state.totalPages = Math.ceil(res.data.total / state.pageInfo.limit);
+            }
         });
     }
 
