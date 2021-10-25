dfc87dbbc63a71ed73846285d2945f2b7eb2920f
qiang.zhao <qiang.zhao@vhall.com>
2021-10-25 10:32:32 +0800

    登錄

--- src/domain/user/user.server.js
+++ src/domain/user/user.server.js
@@ -1,9 +1,18 @@
+import requestApi from '../../request/index.js';
+import '../../libs/sdk.js'
+
 export default function useUserServer() {
     const state = {}
 
-    const roleLogin = () => {
-
+    // 验证码登录&&账号登录
+    const loginInfo = (data) => {
+        return requestApi.roomBase.loginInfo(data)
     }
 
-    return { state, roleLogin }
+    // 第三方授权
+    const callbackUserInfo = (data) => {
+        return requestApi.roomBase.callbackUserInfo(data)
+    }
+
+    return { state, loginInfo, callbackUserInfo }
 }
