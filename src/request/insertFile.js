aafcfa776dbd869c48991a1c02ed732bdbbfc96d
xiaodong.ding <xiaodong.ding@vhall.com>
2021-10-26 17:03:59 +0800

    插播完善

--- src/request/insertFile.js
+++ src/request/insertFile.js
@@ -10,8 +10,10 @@ const getInsertFileList = (params = {}) => {
         is_no_check: params.is_no_check || 1,
         pos: params.pos || 0,
         limit: params.limit || 10,
-        name: params.name || '',
         get_no_trans: params.get_no_trans || 1,
+    }
+    if (params.name) {
+        retParmams.name = params.name
     }
 
     return $http({
@@ -24,7 +26,7 @@ const getInsertFileList = (params = {}) => {
 // 删除插播文件
 const deleteInsertFile = (params = {}) => {
     return $http({
-        url: '/v3/webinars/waiting-file/get-list',
+        url: '/v3/webinars/waiting-file/deletes',
         type: 'POST',
         data: params
     })
