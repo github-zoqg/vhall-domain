9261bb89d43ff2d29812422848faa66e1149ea85
jia.li <jia.li@vhall.com>
2021-11-10 17:40:13 +0800

    feat: biz_id等从外部调用传入，不写死。去除无效代码

--- src/request/user.js
+++ src/request/user.js
@@ -1,72 +1,47 @@
 import $http from '@/utils/http.js'
-import { merge } from '../utils/index.js'
-import contextServer from "../domain/common/context.server"
 
 // 账号登录&&验证码登录
 const loginInfo = (params = {}) => {
-    // const { state } = contextServer.get('roomInitGroupServer') || {}
-
-    const retParmams = params
-    retParmams.biz_id = 2
-
     return $http({
         url: '/v3/users/user-consumer/login',
         type: 'POST',
-        data: retParmams
+        data: params
     })
 }
 
 // 第三方授权
 const callbackUserInfo = (params = {}) => {
-    // const { state } = contextServer.get('roomBaseServer')
-
-    const retParmams = params;
-
     return $http({
         url: '/v3/users/oauth/callback',
         type: 'POST',
-        data: retParmams
+        data: params
     })
 }
 
 // 注册
 const register = (params = {}) => {
-    // const { state } = contextServer.get('roomBaseServer')
-
-    const retParmams = { ...params }
-
     return $http({
         url: '/v3/users/user-consumer/register',
         type: 'POST',
-        data: retParmams
+        data: params
     })
 }
 
 // 手机||邮箱验证码
 const codeCheck = (params = {}) => {
-    const { state } = contextServer.get('roomBaseServer')
-
-    const retParmams = { ...params }
-
     return $http({
         url: '/v3/users/code-consumer/check',
         type: 'POST',
-        data: retParmams
+        data: params
     })
 }
 
 // 密码重置
 const resetPassword = (params = {}) => {
-    const { state } = contextServer.get('roomInitGroupServer')
-
-    const retParmams = Object.assign({},params,{
-        biz_id: state.biz_id || 2
-    })
-
     return $http({
         url: '/v3/users/user-consumer/reset-password',
         type: 'POST',
-        data: retParmams
+        data: params
     })
 }
 
