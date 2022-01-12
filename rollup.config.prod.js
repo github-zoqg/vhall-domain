4b1ac955ec566f9d4b8642b76d8929b4307be47c
xinyuan.yang <xinyuan.yang@vhall.com>
2022-01-12 16:37:13 +0800

    modify: 命名修改

--- rollup.config.prod.js
+++ rollup.config.prod.js
@@ -1,41 +1,37 @@
 import babel from 'rollup-plugin-babel';
 import commonjs from 'rollup-plugin-commonjs';
-import {uglify} from "rollup-plugin-uglify";
+import { uglify } from 'rollup-plugin-uglify';
 import alias from '@rollup/plugin-alias';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 
-import path from 'path'
-
-
+import path from 'path';
 
 const inputMapList = [
-    {
-        input: 'src/index.js',
-        output: [
-            {
-                file: './dist/lib/middleDomain.js',
-                format: 'umd',
-                name:'middleDomain',
-                sourcemap:true
-            }
-        ]
-    }
+  {
+    input: 'src/index.js',
+    output: [
+      {
+        file: './dist/lib/middle-domain.js',
+        format: 'umd',
+        name: 'middleDomain',
+        sourcemap: true
+      }
+    ]
+  }
 ];
 const pluginsConfig = [
-    babel({
-        exclude: 'node_modules/**'
-    }),
-    uglify(),
-    commonjs(),
-    nodeResolve({
-        extensions:['.mjs','.js','.json']
-    }),
-    alias({
-        entries: [
-            {find: '@', replacement: path.resolve('./src')},
-        ]
-    }),
+  babel({
+    exclude: 'node_modules/**'
+  }),
+  uglify(),
+  commonjs(),
+  nodeResolve({
+    extensions: ['.mjs', '.js', '.json']
+  }),
+  alias({
+    entries: [{ find: '@', replacement: path.resolve('./src') }]
+  })
 ];
 
-const buildConfig = inputMapList.map(item => Object.assign({}, item, {plugins: pluginsConfig}));
+const buildConfig = inputMapList.map(item => Object.assign({}, item, { plugins: pluginsConfig }));
 export default buildConfig;
