9d1d9b05be53ceecc31e06668e4a01e916df4801
yongkai.shao <yongkai.shao@vhall.com>
2022-01-29 18:26:26 +0800

    feat: 问答server创建

--- src/domain/interactiveTools/qa.server.js
+++ src/domain/interactiveTools/qa.server.js
@@ -1,72 +1,122 @@
-import $http from '@/utils/http.js';
-import requestApi from "@/request/index.js";
-
-const qaApi = requestApi.qa;
+import BaseServer from '@/domain/common/base.server.js';
+import { qa } from '@/request/index.js';
+import { textToEmojiText } from '@/domain/chat/emoji.js';
 
 /**
  * 问答服务
  * @returns
  */
-export default function useQaServer() {
-  let state = {
 
-  };
+class QaServer extends BaseServer {
+  constructor() {
+    if (typeof QaServer.instance === 'object') {
+      return QaServer.instance;
+    }
+    super();
 
+    this.state = {
+      List: [
+        { text: '未回复', count: 0 },
+        { text: '直播中回答', count: 0 },
+        { text: '文字回复', count: 0 },
+        { text: '不处理', count: 0 }
+      ],
+      baseObj: {}, //主办方信息
+      awaitList: [], // 待处理
+      textDealList: [], // 文字回复
+      audioList: [], // 直播中回答
+      noDealList: [], // 不处理
+      activeObj: {} // 当前正在展示的信息
+    };
 
-  function v3InitChatMess(params= {}) {
-    return qaApi.list.v3InitChatMess(params).then(res => {
-      console.log("qaApi v3InitChatMess--------->", res);
+    QaServer.instance = this;
+    return this;
+  }
+
+  InitChatMess(params = {}) {
+    return qa.list.InitChatMess(params).then(res => {
+      if (res.code == 200) {
+        this.state.baseObj = res.data;
+        sessionStorage.setItem('interact_token', res.data.interact.interact_token);
+      }
       return res;
     });
   }
 
-  function v3GetQa(params = {}) {
-    return requestApi.qa.list.v3GetQa(params);
+  v3GetQa(params = {}) {
+    return qa.list.v3GetQa(params);
   }
 
-  function v3CloseQa(params = {}) {
-    return requestApi.qa.list.v3CloseQa(params);
+  v3CloseQa(params = {}) {
+    return qa.list.v3CloseQa(params);
   }
 
-  function v3GetQaNum(params = {}) {
-    return requestApi.qa.list.v3CloseQa(params);
+  v3GetQaNum(params = {}) {
+    return qa.list.v3GetQaNum(params);
   }
 
-  function v3ReplayUserQu(params = {}) {
-    return requestApi.qa.list.v3CloseQa(params);
+  v3ReplayUserQu(params = {}) {
+    return qa.list.v3ReplayUserQu(params);
   }
 
-  function getAutherQa(params = {}) {
-    return requestApi.qa.list.v3CloseQa(params);
+  getAutherQa(params = {}) {
+    return qa.list.getAutherQa(params).then(res => {
+      if (res.code == 200) {
+        if (res.data && res.data.list) {
+          res.data.list.forEach(item => {
+            if (item.content) {
+              item.content = textToEmojiText(item.content);
+            }
+          });
+
+          const { type } = params;
+
+          switch (type) {
+            case 0:
+              this.state.awaitList = res.data.list;
+              this.state.List[0].count = res.data.total;
+              this.state.activeObj = Object.assign(this.state.activeObj, {
+                active: 0,
+                count: res.data.total
+              });
+              break;
+            case 1:
+              this.state.noDealList = res.data.list;
+              this.state.List[3].count = res.data.total;
+              this.state.activeObj.count = res.data.total;
+              break;
+            case 2:
+              this.state.audioList = res.data.list;
+              this.state.List[1].count = res.data.total;
+              this.state.activeObj.count = res.data.total;
+              break;
+          }
+        }
+      }
+      return res;
+    });
   }
 
-  function sendPrivateMsg(params = {}) {
-    return requestApi.qa.list.v3CloseQa(params);
+  sendPrivateMsg(params = {}) {
+    return qa.list.sendPrivateMsg(params);
   }
 
-  function v3GetTextReply(params = {}) { }
+  v3GetTextReply(params = {}) {}
 
-  function v3Revoke(params = {}) { }
+  v3Revoke(params = {}) {}
 
-  function v3GetPrivateList(params = {}) { }
+  getPrivateList(params = {}) {
+    return qa.list.getPrivateList(params).then(res => {
+      this.state.priteChatList = res.data.list;
+      return res;
+    });
+  }
 
-  function v3GetPrivCon(params = {}) { }
+  v3GetPrivCon(params = {}) {}
 
-  function v3SetUser(params = {}) { }
+  v3SetUser(params = {}) {}
+}
 
-  return {
-    state,
-    v3InitChatMess,
-    v3GetQa,
-    v3CloseQa,
-    v3GetQaNum,
-    v3ReplayUserQu,
-    getAutherQa,
-    sendPrivateMsg,
-    v3GetTextReply,
-    v3Revoke,
-    v3GetPrivateList,
-    v3GetPrivCon,
-    v3SetUser
-  };
+export default function useQaServer() {
+  return new QaServer();
 }
--- src/request/qa/list/index.js
+++ src/request/qa/list/index.js
@@ -1,168 +1,158 @@
-import $http from '@/utils/http.js'
-import contextServer from "@/domain/common/context.server.js"
+import request from '@/utils/http.js';
+import env from '@/request/env';
+import contextServer from '@/domain/common/context.server.js';
 
 // 初始化问答信息----予以提供聊天实例化
-const v3InitChatMess = (params = {}) => {
-  // const { state } = contextServer.get('roomBaseServer')
-
-  let retParams = {
-    // room_id: params.room_id || state.watchInitData.interact.room_id,
-  }
-  retParams = Object.assign(retParams, params)
-
-  return $http({
-    url: '/v3/webinars/live/get-connect',
-    type: 'GET',
-    data: retParams
-  })
+function InitChatMess(params) {
+  const url = env.qa === 'v3' ? '/v3/webinars/live/get-connect' : '';
+  let retParams = {};
+  retParams = Object.assign(retParams, params);
+  console.log('retParams------->', retParams);
+  return request({
+    url: url,
+    method: 'GET',
+    params: retParams
+  });
 }
 
 // 主持人开启问答
 const v3GetQa = (params = {}) => {
   let retParams = {
-    room_id: params.room_id || state.watchInitData.interact.room_id,
-  }
-  retParams = Object.assign(retParams, params)
+    room_id: params.room_id || state.watchInitData.interact.room_id
+  };
+  retParams = Object.assign(retParams, params);
 
-  return $http({
+  return request({
     url: '/v3/interacts/qa/enable',
-    type: 'POST',
+    method: 'POST',
     data: retParams
-  })
-}
+  });
+};
 
 // 主持人关闭问答
 const v3CloseQa = (params = {}) => {
   let retParams = {
-    room_id: params.room_id || state.watchInitData.interact.room_id,
-  }
-  retParams = Object.assign(retParams, params)
-  return $http({
+    room_id: params.room_id || state.watchInitData.interact.room_id
+  };
+  retParams = Object.assign(retParams, params);
+  return request({
     url: '/v3/interacts/qa/enable',
-    type: 'POST',
+    method: 'POST',
     data: retParams
-  })
-}
+  });
+};
 
 // 获取当前场次收集到的问题个数
 const v3GetQaNum = (params = {}) => {
   let retParams = {
-    room_id: params.room_id || state.watchInitData.interact.room_id,
-  }
-  retParams = Object.assign(retParams, params)
-  return $http({
+    room_id: params.room_id || state.watchInitData.interact.room_id
+  };
+  retParams = Object.assign(retParams, params);
+  return request({
     url: '/v3/interacts/qa/get-current-play-question-num',
-    type: 'POST',
+    method: 'POST',
     data: retParams
-  })
-}
+  });
+};
 
 // 获取当前场次收集到的历史问答 主持人
 const v3ReplayUserQu = (params = {}) => {
   let retParams = {
-    room_id: params.room_id || state.watchInitData.interact.room_id,
-  }
-  retParams = Object.assign(retParams, params)
-  return $http({
+    room_id: params.room_id || state.watchInitData.interact.room_id
+  };
+  retParams = Object.assign(retParams, params);
+  return request({
     url: '/v3/interacts/qa/reply-user-question',
-    type: 'POST',
+    method: 'POST',
     data: retParams
-  })
-}
+  });
+};
 
 // 提问列表
 const getAutherQa = (params = {}) => {
-  let retParams = {
-    room_id: params.room_id || state.watchInitData.interact.room_id,
-  }
-  retParams = Object.assign(retParams, params)
-  return $http({
+  return request({
     url: '/v3/interacts/qa/get-question-by-status',
-    type: 'POST',
-    data: retParams
-  })
-}
+    method: 'POST',
+    data: params
+  });
+};
 
 // 发送私聊信息
 const sendPrivateMsg = (params = {}) => {
   let retParams = {
-    room_id: params.room_id || state.watchInitData.interact.room_id,
-  }
-  retParams = Object.assign(retParams, params)
-  return $http({
+    room_id: params.room_id || state.watchInitData.interact.room_id
+  };
+  retParams = Object.assign(retParams, params);
+  return request({
     url: '/v3/interacts/chat-private/send-message',
-    type: 'POST',
+    method: 'POST',
     data: retParams
-  })
-}
+  });
+};
 
 // 文字回复---私密
 const v3GetTextReply = (params = {}) => {
   let retParams = {
-    room_id: params.room_id || state.watchInitData.interact.room_id,
-  }
-  retParams = Object.assign(retParams, params)
-  return $http({
+    room_id: params.room_id || state.watchInitData.interact.room_id
+  };
+  retParams = Object.assign(retParams, params);
+  return request({
     url: '/v3/interacts/qa/get-answer-list',
-    type: 'POST',
+    method: 'POST',
     data: retParams
-  })
-}
+  });
+};
 
 // 主持人撤销回复
 const v3Revoke = (params = {}) => {
   let retParams = {
-    room_id: params.room_id || state.watchInitData.interact.room_id,
-  }
-  retParams = Object.assign(retParams, params)
-  return $http({
+    room_id: params.room_id || state.watchInitData.interact.room_id
+  };
+  retParams = Object.assign(retParams, params);
+  return request({
     url: '/v3/interacts/qa/revoke-reply',
-    type: 'POST',
+    method: 'POST',
     data: retParams
-  })
-}
+  });
+};
 
 // 获取私聊列表
-const v3GetPrivateList = (params = {}) => {
-  let retParams = {
-    room_id: params.room_id || state.watchInitData.interact.room_id,
-  }
-  retParams = Object.assign(retParams, params)
-  return $http({
+const getPrivateList = (params = {}) => {
+  return request({
     url: '/v3/interacts/chat-private/get-rank-list',
-    type: 'POST',
-    data: retParams
-  })
-}
+    method: 'POST',
+    data: params
+  });
+};
 
 // 获取私聊内容
 const v3GetPrivCon = (params = {}) => {
   let retParams = {
-    room_id: params.room_id || state.watchInitData.interact.room_id,
-  }
-  retParams = Object.assign(retParams, params)
-  return $http({
+    room_id: params.room_id || state.watchInitData.interact.room_id
+  };
+  retParams = Object.assign(retParams, params);
+  return request({
     url: '/v3/interacts/chat-private/get-list',
-    type: 'POST',
+    method: 'POST',
     data: retParams
-  })
-}
+  });
+};
 
 // 设置联系人列表
 const v3SetUser = (params = {}) => {
   let retParams = {
-    room_id: params.room_id || state.watchInitData.interact.room_id,
-  }
-  retParams = Object.assign(retParams, params)
-  return $http({
+    room_id: params.room_id || state.watchInitData.interact.room_id
+  };
+  retParams = Object.assign(retParams, params);
+  return request({
     url: '/v3/interacts/chat-private/set-rank-list',
-    type: 'POST',
+    method: 'POST',
     data: retParams
-  })
-}
+  });
+};
 
 export default {
-  v3InitChatMess,
+  InitChatMess,
   v3GetQa,
   v3CloseQa,
   v3GetQaNum,
@@ -171,7 +161,7 @@ export default {
   sendPrivateMsg,
   v3GetTextReply,
   v3Revoke,
-  v3GetPrivateList,
+  getPrivateList,
   v3GetPrivCon,
   v3SetUser
-}
+};
