118949c18e0a3ff01c9467b22f9f6fb3d887cbfb
yongkai.shao <yongkai.shao@vhall.com>
2022-02-09 10:20:02 +0800

    修改问答管理

--- src/domain/interactiveTools/qa.server.js
+++ src/domain/interactiveTools/qa.server.js
@@ -1,6 +1,11 @@
 import BaseServer from '@/domain/common/base.server.js';
+import VhallPaasSDK from '@/sdk/index.js';
+import useMsgServer from '@/domain/common/msg.server';
 import { qa } from '@/request/index.js';
-import { textToEmojiText } from '@/domain/chat/emoji.js';
+import { textToEmojiText, textToEmoji } from '@/domain/chat/emoji.js';
+
+//消息服务
+const msgServer = useMsgServer();
 
 /**
  * 问答服务
@@ -15,6 +20,35 @@ class QaServer extends BaseServer {
     super();
 
     this.state = {
+      activeIndex: 0, // 当前正在展示的Dom
+      textDialogStatus: false, // 是否显示输入框
+      // 当前展示 提交信息集合
+      sendMessage: {
+        text: '',
+        Radio: '1' // 信息类型
+      },
+      isSearch: false, // 是否是正在搜索数据
+      // testAnswer: 2,
+      // testAnswerSelects: [
+      //   {
+      //     label: '全部回答',
+      //     value: 2
+      //   },
+      //   {
+      //     label: '公开回答',
+      //     value: 1
+      //   },
+      //   {
+      //     label: '悄悄回答',
+      //     value: 0
+      //   }
+      // ],
+      exactSearch: {
+        exactSearch0: '', // 待处理检索
+        exactSearch1: '', // 直播中问答检索
+        exactSearch2: '', // 文字回复检索
+        exactSearch3: '' // 未处理检索
+      },
       List: [
         { text: '未回复', count: 0 },
         { text: '直播中回答', count: 0 },
@@ -26,15 +60,110 @@ class QaServer extends BaseServer {
       textDealList: [], // 文字回复
       audioList: [], // 直播中回答
       noDealList: [], // 不处理
-      activeObj: {} // 当前正在展示的信息
+      activeObj: {}, // 当前正在展示的信息
+      onlyChatMess: {}, // 当前私聊对象
+      priteChatList: [] // 私聊列表
     };
+
+    this.listenEvents();
+
+    this.chatInstance = null; // 聊天实例
+    // this.privateChat = null;
 
     QaServer.instance = this;
     return this;
   }
 
-  InitChatMess(params = {}) {
-    return qa.list.InitChatMess(params).then(res => {
+  // 修改数据
+  setState(key, value) {
+    this.state[key] = value;
+  }
+
+  //监听msgServer通知
+  listenEvents() {
+    msgServer.$onMsg('question_answer_create', rawMsg => {
+      // 发起端收到消息
+      rawMsg.content = this.emojiToText(rawMsg.content);
+      this.awaitList.push(e);
+      this.List[0].count = this.awaitList.length;
+      console.warn('domain qa listenEvents------>', this.awaitList);
+    });
+  }
+
+  emojiToText(content) {
+    return textToEmoji(content)
+      .map(c => {
+        return c.msgType == 'text'
+          ? c.msgCont
+          : `<img width="24" src="${c.msgImage}" border="0" />`;
+      })
+      .join(' ');
+  }
+
+  // 初始化聊天实例
+  async initChatInstance(params = {}, callbackFn) {
+    const options = {
+      appId: this.state.baseObj.interact.paas_app_id, // appId 必须
+      accountId: this.state.baseObj.join_info.third_party_user_id, // 第三方用户ID
+      channelId: this.state.baseObj.interact.channel_id, // 频道id 必须
+      token: this.state.baseObj.interact.paas_access_token, // 必须， token，初始化接口获取
+      hide: true
+    };
+    const vhallchat = await VhallPaasSDK.modules.VhallChat.createInstance(options);
+    this.chatInstance = vhallchat.message; // 聊天实例句柄
+
+    console.log('this.chatInstance--------->', this.chatInstance);
+    // VhallPaasSDK.modules.VhallChat.createInstance(
+    //   option,
+    //   event => {
+    //     this.chatInstance = event.message; // 聊天实例句柄
+
+    //     console.log('this.chatInstance--------->', this.chatInstance);
+
+    //     if (callbackFn) {
+    //       callbackFn(event);
+    //     }
+    //   },
+    //   err => {
+    //     console.error('domain qa initChatInstance-------->', err);
+    //   }
+    // );
+  }
+
+  // 根据问题id查询索引值
+  static _findMsgItemByList(data, question_id) {
+    let index = data.findIndex(item => {
+      return item.id == question_id;
+    });
+    return index;
+  }
+
+  /**
+   * 文本内容回复
+   * @param {*} item 回复的当前内容
+   * @param {*} index 回复的信息索引
+   */
+  replayIsText(item, index) {
+    this.state.sendMessage.text = '';
+    this.state.sendMessage.Radio = '1';
+    this.state.sendMessage = Object.assign(this.state.sendMessage, item, {
+      activeDom: this.state.activeIndex,
+      index: index
+    });
+    this.state.textDialogStatus = true;
+    console.log('qa domain replayIsText-------->', this.state);
+  }
+
+  handlerAnswer(status) {
+    if (status == 'public') {
+      this.state.sendMessage.Radio = 1;
+    } else {
+      this.state.sendMessage.Radio = 0;
+    }
+  }
+
+  initChatMess(params = {}) {
+    return qa.list.initChatMess(params).then(res => {
       if (res.code == 200) {
         this.state.baseObj = res.data;
         sessionStorage.setItem('interact_token', res.data.interact.interact_token);
@@ -43,24 +172,63 @@ class QaServer extends BaseServer {
     });
   }
 
-  v3GetQa(params = {}) {
-    return qa.list.v3GetQa(params);
+  qaEnable(params = {}) {
+    return qa.list.qaEnable(params);
   }
 
-  v3CloseQa(params = {}) {
-    return qa.list.v3CloseQa(params);
+  qaDisable(params = {}) {
+    return qa.list.qaDisable(params);
   }
 
-  v3GetQaNum(params = {}) {
-    return qa.list.v3GetQaNum(params);
+  getCurrentPlayQuestionNum(params = {}) {
+    return qa.list.getCurrentPlayQuestionNum(params);
   }
 
-  v3ReplayUserQu(params = {}) {
-    return qa.list.v3ReplayUserQu(params);
+  replyUserQuestion(params = {}) {
+    console.log('qa domain replyUserQuestion-------->', params, this.state.awaitList);
+    return qa.list.replyUserQuestion(params).then(res => {
+      if (res.code == 200) {
+        if (params.type == 1) {
+          this.state.List[0].count--;
+          if (this.state.List[0].count <= 0) {
+            this.state.List[0].count = 0;
+          }
+          this.state.List[3].count++;
+          let questionIndex = QaServer._findMsgItemByList(this.state.awaitList, params.question_id);
+          this.state.awaitList.splice(questionIndex, 1);
+        } else if (params.type == 2) {
+          this.state.List[0].count--;
+          let questionIndex = QaServer._findMsgItemByList(this.state.awaitList, params.question_id);
+          this.state.awaitList.splice(questionIndex, 1);
+          this.state.List[1].count++;
+        } else if (params.type == 3) {
+          this.state.textDialogStatus = false;
+          if (this.state.activeIndex != 2) {
+            if (this.state.sendMessage.activeDom == 0) {
+              // 点击的是待处理的 Dom
+              this.state.awaitList.splice(this.state.sendMessage.index, 1);
+            } else if (this.state.sendMessage.activeDom == 1) {
+              // 点击的是直播中回答 Dom
+              this.state.audioList.splice(this.state.sendMessage.index, 1);
+            } else {
+              // 点击的是文字回复 Dom
+              this.state.textDealList.splice(this.state.sendMessage.index, 1);
+            }
+            this.state.List[2].count++;
+            this.state.List[this.state.sendMessage.activeDom].count--;
+            if (this.state.List[this.state.sendMessage.activeDom].count <= 0) {
+              this.state.List[this.state.sendMessage.activeDom].count = 0;
+            }
+          }
+        } else {
+        }
+      }
+      return res;
+    });
   }
 
-  getAutherQa(params = {}) {
-    return qa.list.getAutherQa(params).then(res => {
+  getQuestionByStatus(params = {}) {
+    return qa.list.getQuestionByStatus(params).then(res => {
       if (res.code == 200) {
         if (res.data && res.data.list) {
           res.data.list.forEach(item => {
@@ -75,20 +243,24 @@ class QaServer extends BaseServer {
             case 0:
               this.state.awaitList = res.data.list;
               this.state.List[0].count = res.data.total;
-              this.state.activeObj = Object.assign(this.state.activeObj, {
-                active: 0,
-                count: res.data.total
-              });
+              if (this.state.activeIndex == 0) {
+                this.state.activeObj.count = res.data.total;
+              }
               break;
             case 1:
+              // 不处理消息列表
               this.state.noDealList = res.data.list;
               this.state.List[3].count = res.data.total;
-              this.state.activeObj.count = res.data.total;
+              if (this.state.activeIndex == 3) {
+                this.state.activeObj.count = res.data.total;
+              }
               break;
             case 2:
               this.state.audioList = res.data.list;
               this.state.List[1].count = res.data.total;
-              this.state.activeObj.count = res.data.total;
+              if (this.state.activeIndex == 1) {
+                this.state.activeObj.count = res.data.total;
+              }
               break;
           }
         }
@@ -97,24 +269,63 @@ class QaServer extends BaseServer {
     });
   }
 
-  sendPrivateMsg(params = {}) {
-    return qa.list.sendPrivateMsg(params);
+  chatPrivateSendMessage(params = {}) {
+    return qa.list.chatPrivateSendMessage(params);
   }
 
-  v3GetTextReply(params = {}) {}
-
-  v3Revoke(params = {}) {}
-
-  getPrivateList(params = {}) {
-    return qa.list.getPrivateList(params).then(res => {
-      this.state.priteChatList = res.data.list;
+  getTextReply(params = {}) {
+    return qa.list.getTextReply(params).then(res => {
+      if (res.code == 200) {
+        try {
+          res.data.list.forEach(item => {
+            if (item.content) {
+              item.content = textToEmojiText(item.content);
+            }
+          });
+        } catch (error) {
+          console.warn(error, '聊天消息过滤错误');
+        }
+        this.state.textDealList = res.data.list;
+        this.state.List[2].count = res.data.total;
+        if (this.state.activeIndex == 2) {
+          this.state.activeObj.count = res.data.total;
+        }
+      }
       return res;
     });
   }
 
-  v3GetPrivCon(params = {}) {}
+  // 撤销回复
+  revokeReply(params = {}) {
+    return qa.list.revokeReply(params).then(res => {
+      if (res.code == 200) {
+        this.state.textDealList[params.father_index].answer[params.index].is_backout = 1;
+      }
+      return res;
+    });
+  }
 
-  v3SetUser(params = {}) {}
+  chatPrivateGetRankList(params = {}) {
+    return qa.list.chatPrivateGetRankList(params).then(res => {
+      this.state.priteChatList = res.data.list;
+      if (!(this.state.onlyChatMess && this.state.onlyChatMess.type)) {
+        this.state.onlyChatMess = this.state.priteChatList[0];
+      }
+      return res;
+    });
+  }
+
+  chatPrivateGetList(params = {}) {
+    return qa.list.chatPrivateGetList(params).then(res => {
+      return res;
+    });
+  }
+
+  setRankList(params = {}) {
+    return qa.list.setRankList(params).then(res => {
+      return res;
+    });
+  }
 }
 
 export default function useQaServer() {
