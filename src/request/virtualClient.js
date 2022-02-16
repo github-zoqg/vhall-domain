8a546b5f43b3e4d3592a5543b4b3a88713c8e7c6
zhangxiao <215906340>
2022-02-16 14:52:34 +0800

    feat: 重新封装虚拟人数

--- src/request/virtualClient.js
+++ src/request/virtualClient.js
@@ -1,43 +1,35 @@
 import request from '@/utils/http.js';
-import { merge } from '../utils/index.js';
-import contextServer from '../domain/common/context.server';
 
 // 发起端-开始增加虚拟观众
 const virtualClientStart = (params = {}) => {
-  // const { state } = contextServer.get('roomInitGroupServer') || {}
-
   const retParmams = params;
 
   return request({
     url: '/v3/webinars/virtual/start',
     method: 'GET',
-    data: retParmams
+    params: retParmams
   });
 };
 
 // 发起端-增加虚拟观众
 const virtualAccumulation = (params = {}) => {
-  // const { state } = contextServer.get('roomInitGroupServer') || {}
-
   const retParmams = params;
 
   return request({
     url: '/v3/webinars/virtual/accumulation',
     method: 'GET',
-    data: retParmams
+    params: retParmams
   });
 };
 
 // 发起端-获取虚拟观众基数
 const virtualClientGet = (params = {}) => {
-  // const { state } = contextServer.get('roomInitGroupServer') || {}
-
   const retParmams = params;
 
   return request({
     url: '/v3/webinars/virtual/get-base',
     method: 'GET',
-    data: retParmams
+    params: retParmams
   });
 };
 
