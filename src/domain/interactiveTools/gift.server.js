f099b58bb157180a1557c2dba62e643285d316c7
yongkai.shao <yongkai.shao@vhall.com>
2022-02-19 18:32:14 +0800

    feat: 修改礼物消息

--- src/domain/interactiveTools/gift.server.js
+++ src/domain/interactiveTools/gift.server.js
@@ -7,29 +7,43 @@ import useMsgServer from '@/domain/common/msg.server.js';
 import { giftsApi } from '@/request/index.js';
 class giftsServer extends BaseServer {
   constructor() {
+    if (typeof giftsServer.instance === 'object') {
+      return giftsServer.instance;
+    }
     super();
+
     this.state = {};
+
+    this.listenMsg();
+
+    giftsServer.instance = this;
+    return this;
   }
 
   setState(key, value) {
     this.state[key] = value;
   }
 
+  /**
+   * 监听消息服务
+   */
   listenMsg() {
     // 房间消息
     useMsgServer().$onMsg('ROOM_MSG', rawMsg => {
       let temp = Object.assign({}, rawMsg);
-
       if (typeof temp.data !== 'object') {
         temp.data = JSON.parse(temp.data);
         temp.context = JSON.parse(temp.context);
       }
-      console.log(temp, '原始消息');
+      console.log('domain giftsServer---------->', temp);
+
       const { type = '' } = temp.data || {};
       switch (type) {
-        // 计时器开始
-        case 'timer_start':
+        case 'timer_start': // 计时器开始
           this.$emit('timer_start', temp);
+          break;
+        case 'gift_send_success': //接收送礼物消息
+          this.$emit('gift_send_success', temp);
           break;
         default:
           break;
