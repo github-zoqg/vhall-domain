b89c501d01db059dbf88c04c4c4ce154ce0afb6e
qingzhao.xu <qingzhao.xu@vhall.com>
2022-02-26 16:28:17 +0800

    feat: 观看端红包

--- src/domain/interactiveTools/redPacketServer.js
+++ src/domain/interactiveTools/redPacketServer.js
@@ -7,6 +7,12 @@ const RED_ENVELOPE_OK = 'red_envelope_ok'; // 支付成功消息
 class RedPacketServer extends BaseServer {
   constructor() {
     super();
+    this._uuid = '';
+    this.state = {
+      info: {},
+      amount: 0,
+      status: -1 // 红包状态: 0 抢光了
+    };
     this.listenMsg();
   }
 
@@ -14,11 +20,49 @@ class RedPacketServer extends BaseServer {
     useMsgServer().$onMsg('ROOM_MSG', msg => {
       switch (msg.data.event_type || msg.data.type) {
         case RED_ENVELOPE_OK:
-          this.$emit(RED_ENVELOPE_OK, msg);
+          this.$emit(RED_ENVELOPE_OK, msg.data);
           break;
       }
     });
   }
+  /**
+   * @description 获取红包(观看端的初始化)
+   */
+  getRedPacketInfo(uuid) {
+    const { watchInitData } = useRoomBaseServer().state;
+    const { interact } = watchInitData;
+    this._uuid = uuid;
+    return redPacketApi
+      .getRedPacketInfo({
+        room_id: interact.room_id,
+        red_packet_uuid: this._uuid
+      })
+      .then(res => {
+        this.state.info = res.data.red_packet;
+        this.state.status = res.data.status;
+        if (res.data.amount) {
+          this.state.amount = res.data.amount;
+        }
+        return res;
+      });
+  }
+  /**
+   * @description 领红包
+   */
+  openRedPacket() {
+    const { watchInitData } = useRoomBaseServer().state;
+    const { interact } = watchInitData;
+    return redPacketApi
+      .openRedpacket({
+        room_id: interact.room_id,
+        red_packet_uuid: this._uuid
+      })
+      .then(res => {
+        this.state.amount = res.data.amount;
+        return res;
+      });
+  }
+
   /**
    * @description 发送红包
    */
@@ -27,6 +71,19 @@ class RedPacketServer extends BaseServer {
     const { interact } = watchInitData;
     return redPacketApi.createRedpacket({
       room_id: interact.room_id,
+      ...params
+    });
+  }
+  /**
+   * @description 红包中奖人列表
+   */
+  getRedPacketWinners(params) {
+    const { watchInitData } = useRoomBaseServer().state;
+    const { interact } = watchInitData;
+    return redPacketApi.getRedPacketWinners({
+      order: 'created_at',
+      room_id: interact.room_id,
+      red_packet_uuid: this._uuid,
       ...params
     });
   }
--- src/request/redPacket.js
+++ src/request/redPacket.js
@@ -4,7 +4,7 @@
 
 import request from '@/utils/http.js';
 
-// 查询活动配置信息
+// 发送红包
 const createRedpacket = params => {
   return request({
     url: '/v3/interacts/redpacket/create',
@@ -12,7 +12,32 @@ const createRedpacket = params => {
     data: params
   });
 };
+// 获取红包信息
+const getRedPacketInfo = params => {
+  return request({
+    url: '/v3/interacts/redpacket/get-redpacket-status',
+    method: 'POST',
+    data: params
+  });
+};
+// 开红包
+const openRedpacket = params => {
+  return request('/v3/interacts/redpacket/open-redpacket', {
+    method: 'POST',
+    data: params
+  });
+};
+// 红包获奖人历史
+const getRedPacketWinners = params => {
+  return request('/v3/interacts/redpacket/get-redpacket-partition-recorder', {
+    method: 'POST',
+    data: params
+  });
+};
 
 export default {
-  createRedpacket
+  createRedpacket,
+  getRedPacketInfo,
+  openRedpacket,
+  getRedPacketWinners
 };
