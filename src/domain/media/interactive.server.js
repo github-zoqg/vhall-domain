dd9497b74f23ad94f5f2e624869319a3d5f30242
xiaodong.ding <xiaodong.ding@vhall.com>
2022-02-28 15:51:50 +0800

    fix: 桌面共享报错

--- src/domain/media/interactive.server.js
+++ src/domain/media/interactive.server.js
@@ -131,18 +131,18 @@ class InteractiveServer extends BaseServer {
       broadcastConfig:
         watchInitData.join_info.role_name == 1
           ? {
-              adaptiveLayoutMode:
-                VhallPaasSDK.modules.VhallRTC[sessionStorage.getItem('layout')] ||
-                VhallPaasSDK.modules.VhallRTC.CANVAS_ADAPTIVE_LAYOUT_GRID_MODE, // 旁路布局，选填 默认大屏铺满，一行5个悬浮于下面
-              profile: VhallPaasSDK.modules.VhallRTC.BROADCAST_VIDEO_PROFILE_1080P_1, // 旁路直播视频质量参数
-              paneAspectRatio: VhallPaasSDK.modules.VhallRTC.BROADCAST_PANE_ASPACT_RATIO_16_9, //旁路混流窗格指定高宽比。  v2.3.2及以上
-              precastPic: false, // 选填，当旁路布局模板未填满时，剩余的窗格默认会填充系统默认小人图标。可配置是否显示此图标。
-              border: {
-                // 旁路边框属性
-                width: 2,
-                color: '0x1a1a1a'
-              }
+            adaptiveLayoutMode:
+              VhallPaasSDK.modules.VhallRTC[sessionStorage.getItem('layout')] ||
+              VhallPaasSDK.modules.VhallRTC.CANVAS_ADAPTIVE_LAYOUT_GRID_MODE, // 旁路布局，选填 默认大屏铺满，一行5个悬浮于下面
+            profile: VhallPaasSDK.modules.VhallRTC.BROADCAST_VIDEO_PROFILE_1080P_1, // 旁路直播视频质量参数
+            paneAspectRatio: VhallPaasSDK.modules.VhallRTC.BROADCAST_PANE_ASPACT_RATIO_16_9, //旁路混流窗格指定高宽比。  v2.3.2及以上
+            precastPic: false, // 选填，当旁路布局模板未填满时，剩余的窗格默认会填充系统默认小人图标。可配置是否显示此图标。
+            border: {
+              // 旁路边框属性
+              width: 2,
+              color: '0x1a1a1a'
             }
+          }
           : {}, // 自动旁路   开启旁路直播方法所需参数
       otherOption: watchInitData.report_data
     };
@@ -737,6 +737,7 @@ class InteractiveServer extends BaseServer {
 
   // 获取插播和桌面共享的流信息
   getDesktopAndIntercutInfo() {
+    if (!this.interactiveInstance) return
     let streamList = this.interactiveInstance.getRoomStreams();
     streamList = streamList.map(stream => ({
       ...stream,
