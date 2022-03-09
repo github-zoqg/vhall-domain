6f18ad9231c2b8cd7eca301efdaaf25faa187330
xinyuan.yang <xinyuan.yang@vhall.com>
2022-03-09 21:31:14 +0800

    chore: 修改的bug被其它人覆盖,还原

--- src/domain/group/StandardGroupServer.js
+++ src/domain/group/StandardGroupServer.js
@@ -1,11 +1,17 @@
 import BaseServer from '../common/base.server';
 import useRoomBaseServer from '../room/roombase.server';
+<<<<<<< Updated upstream
 import useInteractiveServer from '../media/interactive.server';
 import useMsgServer from '../common/msg.server';
 import useDocServer from '../doc/doc.server';
 import { group as groupApi } from '../../request/index.js';
 import { isPc, sleep } from '@/utils/index.js';
 
+=======
+import useMsgServer from '../common/msg.server';
+import { group as groupApi } from '../../request/index.js';
+import { isPc } from '@/utils/index.js';
+>>>>>>> Stashed changes
 /**
  * 标准分组直播场景下的分组相关服务
  *
@@ -29,7 +35,11 @@ class StandardGroupServer extends BaseServer {
         // join_role: "1"  //参会角色
         // main_screen: "16423152" //主屏账户Id
         // name: "分组1"
+<<<<<<< Updated upstream
         // presentation_screen: "16423152" // 演示者ID
+=======
+        // presentation_screen: "16423152"
+>>>>>>> Stashed changes
         // speaker_list: []   //上麦列表
         //    ------nick_name  //昵称
         //    ------role_name  //1主持人|2观众|3助理|4嘉宾|5admin|20组长
@@ -836,6 +846,7 @@ class StandardGroupServer extends BaseServer {
     }
   }
 
+<<<<<<< Updated upstream
   /**
    * 收到切换小组消息,判断是否需要切换 channel
    * 服务端无法确定是group_ids数组的id的 from,to 顺序
@@ -890,6 +901,20 @@ class StandardGroupServer extends BaseServer {
 
   // 设置主讲人（邀请演示）
   async presentation() {
+=======
+  // 结束别的用户演示
+  async endOtherPresentation() {
+    const { watchInitData } = useRoomBaseServer().state;
+    const params = {
+      room_id: watchInitData.interact.room_id, // 主直播房间ID
+      receive_account_id: ''
+    };
+    return await groupApi.endOtherPresentation(params);
+  }
+
+  // 结束自己演示
+  async endSelfPresentation() {
+>>>>>>> Stashed changes
     const { watchInitData } = useRoomBaseServer().state;
     const params = {
       room_id: watchInitData.interact.room_id // 主直播房间ID
@@ -962,6 +987,67 @@ class StandardGroupServer extends BaseServer {
       clearTimeout(timer)
       this.groupLeaderLeaveMap.delete(msg.sender_id)
     }
+  }
+
+  // 收到切换小组消息,判断是否需要切换 channel
+  async getGroupJoinChangeInfo(group_ids) {
+    // 备份之前的小组信息
+    const oldGroupInitData = JSON.parse(JSON.stringify(this.state.groupInitData));
+    console.log('domain -------- oldGroupInitData', oldGroupInitData);
+    // 如果在主直播间，并且 group_ids 中包括主直播间
+    if (!oldGroupInitData.isInGroup && group_ids.indexOf(0) > -1) {
+      // 重新获取最新的 groupInitData
+      await this.init();
+      console.log('domain --------直播间->小组', this.state.groupInitData);
+      // 如果新的小组跟之前的小组不一样则需要关心,否则不需要关心
+      return {
+        isNeedCare: this.state.groupInitData.isInGroup,
+        from: 0,
+        to: this.state.groupInitData.group_id
+      };
+    }
+    // 如果是在小组中，并且 group_ids 中包括了该小组
+    if (oldGroupInitData.isInGroup && group_ids.indexOf(Number(oldGroupInitData.group_id)) > -1) {
+      await this.init();
+      console.log('domain --------小组->直播间', this.state.groupInitData);
+      // 如果现在变为不在小组了,则需要关心
+      if (!this.state.groupInitData.isInGroup) {
+        return {
+          isNeedCare: true,
+          from: oldGroupInitData.group_id,
+          to: 0
+        };
+      }
+      // 如果新的小组跟之前的小组不一样则需要关心,否则不需要关心
+      return {
+        isNeedCare: oldGroupInitData.group_id !== this.state.groupInitData.group_id,
+        from: oldGroupInitData.group_id,
+        to: groupInitData.group_id
+      };
+    }
+    // 如果不满足上述两个 if 则不需要关心
+    return {
+      isNeedCare: false,
+      from: oldGroupInitData.isInGroup ? oldGroupInitData.group_id : 0,
+      to: oldGroupInitData.isInGroup ? oldGroupInitData.group_id : 0
+    };
+  }
+
+  // 分组直播，进出子房间需要在主房间发消息，维护主房间 online-list
+  sendMainRoomJoinChangeMsg(options = { isJoinMainRoom: false, isBanned: false }) {
+    const { watchInitData } = useRoomBaseServer().state;
+    const msgServer = useMsgServer();
+    var isPcClient = isPc();
+    const body = {
+      type: 'main_room_join_change',
+      nickname: watchInitData.join_info.nickname,
+      accountId: watchInitData.join_info.third_party_user_id,
+      isJoinMainRoom: options.isJoinMainRoom,
+      role_name: watchInitData.join_info.role_name,
+      device_type: isPcClient ? '2' : '1',
+      isBanned: options.isBanned
+    };
+    msgServer.sendRoomMsg(JSON.stringify(body));
   }
 }
 
