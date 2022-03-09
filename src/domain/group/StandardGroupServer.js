c35c45de4a9137f9ffd076e8be315e7c2c83979c
xinyuan.yang <xinyuan.yang@vhall.com>
2022-03-09 21:45:26 +0800

    fix: 解决冲突

--- src/domain/group/StandardGroupServer.js
+++ src/domain/group/StandardGroupServer.js
@@ -1,17 +1,11 @@
 import BaseServer from '../common/base.server';
 import useRoomBaseServer from '../room/roombase.server';
-<<<<<<< Updated upstream
 import useInteractiveServer from '../media/interactive.server';
 import useMsgServer from '../common/msg.server';
 import useDocServer from '../doc/doc.server';
 import { group as groupApi } from '../../request/index.js';
 import { isPc, sleep } from '@/utils/index.js';
 
-=======
-import useMsgServer from '../common/msg.server';
-import { group as groupApi } from '../../request/index.js';
-import { isPc } from '@/utils/index.js';
->>>>>>> Stashed changes
 /**
  * 标准分组直播场景下的分组相关服务
  *
@@ -35,11 +29,7 @@ class StandardGroupServer extends BaseServer {
         // join_role: "1"  //参会角色
         // main_screen: "16423152" //主屏账户Id
         // name: "分组1"
-<<<<<<< Updated upstream
         // presentation_screen: "16423152" // 演示者ID
-=======
-        // presentation_screen: "16423152"
->>>>>>> Stashed changes
         // speaker_list: []   //上麦列表
         //    ------nick_name  //昵称
         //    ------role_name  //1主持人|2观众|3助理|4嘉宾|5admin|20组长
@@ -846,7 +836,6 @@ class StandardGroupServer extends BaseServer {
     }
   }
 
-<<<<<<< Updated upstream
   /**
    * 收到切换小组消息,判断是否需要切换 channel
    * 服务端无法确定是group_ids数组的id的 from,to 顺序
@@ -901,20 +890,6 @@ class StandardGroupServer extends BaseServer {
 
   // 设置主讲人（邀请演示）
   async presentation() {
-=======
-  // 结束别的用户演示
-  async endOtherPresentation() {
-    const { watchInitData } = useRoomBaseServer().state;
-    const params = {
-      room_id: watchInitData.interact.room_id, // 主直播房间ID
-      receive_account_id: ''
-    };
-    return await groupApi.endOtherPresentation(params);
-  }
-
-  // 结束自己演示
-  async endSelfPresentation() {
->>>>>>> Stashed changes
     const { watchInitData } = useRoomBaseServer().state;
     const params = {
       room_id: watchInitData.interact.room_id // 主直播房间ID
