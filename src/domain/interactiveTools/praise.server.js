192536d2085e547478bbe1f576d1ca8ee6582be9
zhangxiao <215906340>
2022-03-11 16:01:05 +0800

    feat: 修改点赞消息

--- src/domain/interactiveTools/praise.server.js
+++ src/domain/interactiveTools/praise.server.js
@@ -16,42 +16,46 @@ class PraiseServer extends BaseServer {
     this.state = {
       praiseTotalNum: roomBaseServer.state.priseLike.total
     };
+    this.listenMsg();
   }
   listenMsg() {
     // 房间消息
     msgServer.$onMsg('CHAT', msg => {
-      if (msg.data.type === 'customPraise') {
+      if (msg.data.type === 'permit' && msg.data.event_type == 'customPraiseTotal') {
         this.$emit('customPraise', msg.data);
       }
     });
-    msgServer.$onMsg('CUSTOM_MSG', msg => {
-      if (msg.data.type === 'customPraise') {
-        this.$emit('customPraise', msg.data);
-      }
-    });
+    // 后端发消息，前端不用自己发消息了
+    // msgServer.$onMsg('CUSTOM_MSG', msg => {
+    //   if (msg.data.type === 'customPraise') {
+    //     console.log(msg, '???1234325346')
+    //     // this.$emit('customPraise', msg.data);
+    //   }
+    // });
   }
   // 点赞
   postPraiseIncrement(params = {}) {
     return praiseApi.postPraiseIncrement(params).then(res => {
-      if (res.code == 200) {
-        if (roomBaseServer.state.configList['ui.hide_chat_history'] == '1') {
-          const msgData = {
-            type: 'permit',
-            event_type: 'customPraise',
-            text_content: '',
-            num: params.num,
-            visitorId: roomBaseServer.state.watchInitData.visitor_id
-          };
-          const context = {};
-          msgServer.sendChatMsg(msgData, context);
-          return;
-        }
-        msgServer.sendCustomMsg({
-          type: 'customPraise',
-          num: params.num,
-          visitorId: roomBaseServer.state.watchInitData.visitor_id
-        });
-      }
+      // 后端发消息，前端不用自己发消息了
+      // if (res.code == 200) {
+      //   if (roomBaseServer.state.configList['ui.hide_chat_history'] == '1') {
+      //     const msgData = {
+      //       type: 'permit',
+      //       event_type: 'customPraise',
+      //       text_content: '',
+      //       num: params.num,
+      //       visitorId: roomBaseServer.state.watchInitData.visitor_id
+      //     };
+      //     const context = {};
+      //     msgServer.sendChatMsg(msgData, context);
+      //     return;
+      //   }
+      //   msgServer.sendCustomMsg({
+      //     type: 'customPraise',
+      //     num: params.num,
+      //     visitorId: roomBaseServer.state.watchInitData.visitor_id
+      //   });
+      // }
       return res;
     });
   }
