2227ac50c0cf68febf1152495cecbf787e3af150
xiaodong.ding <xiaodong.ding@vhall.com>
2022-03-11 10:21:05 +0800

    feat: 保存 common-config 第一次调用时传入的参数

--- src/domain/room/roombase.server.js
+++ src/domain/room/roombase.server.js
@@ -344,10 +344,15 @@ class RoomBaseServer extends BaseServer {
 
   // 观看端获取公众号、广告推荐、邀请卡等信息
   getCommonConfig(data = {}) {
+    // 第一次调用的时候存储一下入参，后续如果不传 data 就用第一次保存的参数
+    if (!this._isNotFirstGetCommonConfig) {
+      this._getCommonConfigData = data
+      this._isNotFirstGetCommonConfig = true
+    }
     const defaultParams = {
       webinar_id: this.state.watchInitData.webinar.id
     };
-    const retParams = merge.recursive({}, defaultParams, data);
+    const retParams = merge.recursive({}, defaultParams, data || this._getCommonConfigData);
     return meeting.getCommonConfig(retParams).then(res => {
       if (res.code == 200) {
         this.state.skinInfo = res.data['skin'] ? res.data['skin'].data : {}; // 皮肤信息
