102f0220bc517b3f745a888279f2407b1b0ca58e
yongkai.shao <yongkai.shao@vhall.com>
2022-03-21 15:14:32 +0800

    feat: 修改数据埋点

--- src/domain/index.js
+++ src/domain/index.js
@@ -115,7 +115,7 @@ class Domain {
 
   // 微吼直播产品侧需要的数据
   initVhallReportForProduct(reportOptions) {
-    // window.vhallReportForProduct = new VhallReportForProduct(reportOptions);
+    window.vhallReportForProduct = new VhallReportForProduct(reportOptions);
   }
 }
 const version = '__VERSION__';
