14664ea6a45df2ee15e038e8ddd59a16f042275d
qingzhao.xu <qingzhao.xu@vhall.com>
2022-03-22 21:15:04 +0800

    fix: bug

--- src/domain/interactiveTools/redPacketServer.js
+++ src/domain/interactiveTools/redPacketServer.js
@@ -149,7 +149,12 @@ class RedPacketServer extends BaseServer {
       room_id: interact.room_id,
       red_packet_uuid: this._uuid,
       ...params
-    });
+    }).then(res => {
+      if (res.data.red_packet) {
+        this.state.info = res.data.red_packet
+      }
+      return res
+    })
   }
   /**
    * @description 获取最后一个红包的领取信息
