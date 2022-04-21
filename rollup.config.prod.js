2f92cfd6ee205ff3da65e1c972b8fb36057d2589
xinyuan.yang <xinyuan.yang@vhall.com>
2022-04-21 11:59:26 +0800

    fix: 自己编写rollup插件,用于修改sourcemapUrl

--- rollup.config.prod.js
+++ rollup.config.prod.js
@@ -1,12 +1,14 @@
+import path from 'path';
 import babel from 'rollup-plugin-babel';
 import commonjs from 'rollup-plugin-commonjs';
 import { uglify } from 'rollup-plugin-uglify';
 import alias from '@rollup/plugin-alias';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import replace from '@rollup/plugin-replace';
+import clear from 'rollup-plugin-clear';
+import sourceMapUrl from './scripts/rollup-plugin-source-map-url';
+import copy from 'rollup-plugin-copy'
 const pkg = require('./package.json');
-
-import path from 'path';
 
 const inputMapList = [
   {
@@ -22,6 +24,9 @@ const inputMapList = [
   }
 ];
 const pluginsConfig = [
+  clear({
+    targets: ['dist']
+  }),
   babel({
     exclude: 'node_modules/**'
   }),
@@ -36,6 +41,23 @@ const pluginsConfig = [
   replace({
     preventAssignment: true,
     __VERSION__: pkg.version
+  }),
+  sourceMapUrl({
+    publicPath: 'https://t-vhallsaas-static.oss-cn-beijing.aliyuncs.com/common-static/sourcemap/middle-domain/1.3.15/'
+  }),
+  copy({
+    targets: [
+      {
+        src: `./dist/${pkg.version}/middle-domain.js`,
+        dest: `./dist/cloud/${pkg.version}/`
+      },
+      {
+        src: `./dist/${pkg.version}/middle-domain.js.map`,
+        dest: `./dist/sourcemap/${pkg.version}/`
+      }
+    ],
+    hook: 'writeBundle',
+    verbose: true
   })
 ];
 
