20c8b2d3d0ff9bcbe628861fa3800ddfad0bbe4e
xinyuan.yang <xinyuan.yang@vhall.com>
2022-04-24 19:59:04 +0800

    fix: 文件路径错误

--- src/domain/index.js
+++ src/domain/index.js
@@ -30,7 +30,7 @@ import useKeyLoginServer from '@/domain/keyLogin/keyLogin.server.js';
 import useCustomMenuServer from '@/domain/brand/customMenu.server';
 
 import useRebroadcastServer from '@/domain/interactiveTools/rebroadcast.server';
-import useZIndexServer from '@/domain/interactiveTools/ZIndexServer.js';
+import useZIndexServer from '@/domain/interactiveTools/zindexServer.js';
 
 // 问答关联
 import useQaAdminServer from '@/domain/interactiveTools/qaadmin.server.js';
