db04f0d95ca68659a8e7f7838f9a3c12f293aac7
xiao.zhang <xiao.zhang@vhall.com>
2022-06-28 17:50:22 +0800

    feat: 去掉无用状态

--- src/domain/subscribe/subscribe.server.js
+++ src/domain/subscribe/subscribe.server.js
@@ -15,7 +15,6 @@ class SubscribeServer extends BaseServer {
       subscribeWarmList: [], // 初始化播放器组件个数
       warmVideo: 60,
       isPlaying: false, // 是否播放
-      warmFullScreen: false, // 是否是全屏
       playIndex: 0,  //播放了第几个
       initIndex: 0 // 初始化播放器第几个
     };
