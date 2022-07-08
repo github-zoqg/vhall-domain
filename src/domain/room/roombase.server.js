e0cae3dc225e39dd09f599a9d4c07da785622048
xiao.zhang <xiao.zhang@vhall.com>
2022-07-08 16:12:19 +0800

    feat: 修改

--- src/domain/room/roombase.server.js
+++ src/domain/room/roombase.server.js
@@ -124,10 +124,12 @@ class RoomBaseServer extends BaseServer {
             if (this.state.embedObj.embedVideo) {
               this.state.watchInitData.webinar.no_delay_webinar = 0
             }
-          }
-          // 判断暖场视频数据
-          if (this.state.watchInitData.status === 'subscribe' && res.data.warmup && res.data.warmup.warmup_paas_record_id.length) {
-            this.state.warmUpVideo = res.data.warmup;
+
+            // 判断暖场视频数据
+            if (this.state.watchInitData.status === 'subscribe' && res.data.warmup && res.data.warmup.warmup_paas_record_id.length) {
+              this.state.warmUpVideo = res.data.warmup;
+            }
+
           }
           // 判断是不是第三方推流
           if (res.data.switch && res.data.switch.start_type == 4) {
