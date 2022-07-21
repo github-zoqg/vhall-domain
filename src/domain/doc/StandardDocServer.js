1b3e737fd49960061edfa4043754c50e63cbae0f
xiaodong.ding <xiaodong.ding@vhall.com>
2022-07-21 14:04:50 +0800

    fix: 文档allComplete状态默认值为false

--- src/domain/doc/StandardDocServer.js
+++ src/domain/doc/StandardDocServer.js
@@ -32,7 +32,7 @@ export default class StandardDocServer extends AbstractDocServer {
       pageTotal: 1, //总页数
       pageNum: 1, // 当前页码
 
-      allComplete: true,
+      allComplete: false,
       docLoadComplete: true, // 所有文档是否加载完成
 
       thumbnailList: [], // 缩略图列表
@@ -204,7 +204,7 @@ export default class StandardDocServer extends AbstractDocServer {
         this.state.containerList = []; // 动态容器列表
         this.state.pageTotal = 1; //总页数
         this.state.pageNum = 1; // 当前页码Ï
-        this.state.allComplete = true;
+        this.state.allComplete = false;
         this.state.docLoadComplete = true; // 文档是否加载完成
         this.state.thumbnailList = []; // 缩略图列表
         this.state.switchStatus = false; // 观众是否可见
