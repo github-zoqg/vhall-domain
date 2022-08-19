d4839ab32f5630f9ae57319f489d5fd702a419f4
qingzhao.xu <qingzhao.xu@vhall.com>
2022-08-19 10:47:44 +0800

    feat: 修改saassdk

--- src/sdk/lib/saas-sdk.js
+++ src/sdk/lib/saas-sdk.js
@@ -2,7 +2,6 @@ export default {
   // 数据上报sdk
   report: {
     name: 'VhallReport',
-    // url: '//cnstatic01.e.vhall.com/common-static/middle/middle-log/1.0.6/index.min.js'
-    url: '//t-alistatic01.e.vhall.com/common-static/middle/middle-log/7.4.6-2/index.js'
+    url: '//cnstatic01.e.vhall.com/common-static/middle/middle-log/1.0.7/index.min.js'
   }
 };
