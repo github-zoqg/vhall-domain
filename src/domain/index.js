cf678b77cdcaf77f0fc0038b0eeda6c02ed87b68
itc <itc@126.com>
2022-08-26 17:23:09 +0800

    feat: 埋点

--- src/domain/index.js
+++ src/domain/index.js
@@ -37,6 +37,7 @@ import useZIndexServer from '@/domain/interactiveTools/zindexServer.js';
 import useQaAdminServer from '@/domain/interactiveTools/qaadmin.server.js';
 import useQaServer from '@/domain/interactiveTools/qa.server.js';
 import { setRequestBody, setRequestHeaders } from '@/utils/http.js';
+import { fullLinkBurningPointReport } from '@/utils/report.js'
 import VhallPaasSDK, { ALLSDKCONFIG } from '../sdk';
 import { Dep } from '@/domain/common/base.server';
 import { INIT_DOMAIN } from '@/domain/common/dep.const';
@@ -116,165 +117,28 @@ class Domain {
     window.vhallReport = new VhallReport(reportOptions);
   }
 
-  // 微吼直播【产品侧】需要的数据
-  initVhallReportForProduct(reportOptions) {
-    window.vhallReportForProduct = new VhallReportForProduct(reportOptions);
-    this.fullLinkBurningPointReport(reportOptions);
-  }
+
   // 微吼直播【产品侧】需要的数据(观看端,接入数据信息平台,与发起端区分)
   initVhallReportForWatch(reportOptions) {
     window.vhallReportForWatch = new VhallReportForProduct(reportOptions);
   }
 
-  // ** 微吼直播新加入埋点上报方式（代理，全链路）TODO 会迁移至对应的类库 **
-  fullLinkBurningPointReport(reportOptions) {
+  // 微吼直播【产品侧】需要的数据
+  initVhallReportForProduct(reportOptions) {
+    window.vhallReportForProduct = new VhallReportForProduct(reportOptions);
 
-    // 测试上报开关
-    let isReport = sessionStorage.getItem('isReport');
-    if (!JSON.parse(isReport)) {
-      isReport = confirm("是否开启上报功能");
-      sessionStorage.setItem('isReport', isReport)
-    }
-
-
-    let requestId = 'xxx';
-    let cacheReportCode = {};
-    const useRoomBaseState = useRoomBaseServer().state;
-    const { watchInitData } = useRoomBaseState;
-    const { join_info = {}, webinar = {}, interact = {}, sso = {} } = watchInitData;
-
-    // 生成request_id 规则：用户ID + 活动ID + 时间戳 + 事件ID
-    const randomCode = (type = 0) => {
-      let onlyTimeStamp = new Date().getTime();
-      return `${join_info.third_party_user_id}${webinar.id}${onlyTimeStamp}${type}`;
-    }
-
-    window.vhallFullLinkBurningPointReport = new VhallReportForProduct(reportOptions);
-
-    // 上报地址
-    window.vhallFullLinkBurningPointReport.BASE_URL = reportOptions.env === 'test' ? 'https://t-dc.e.vhall.com/login' : 'https://dc.e.vhall.com/login';
-
-    // 扩展实例后的全局通用上报属性
-    window.vhallFullLinkBurningPointReport.commonParams = {
-      ...{
-        os: 10,
-        // B端账号id
-        business_uid: join_info.third_party_user_id,
-        // 游客ID
-        visitor_id: watchInitData.visitor_id,
-        // 用户唯一id
-        sso_union_id: sso.kick_id,
-        // 用户昵称
-        nickname: encodeURIComponent(join_info.nickname),
-        // 回放ID
-        record_id: webinar.id,
-        // 活动名称(内容名称)
-        webinar_name: webinar.userinfo.nickname,
-        // 是否登录状态
-        is_login: join_info.user_id,
-        // 房间ID
-        room_id: interact.room_id,
-        // 互动房间id
-        inav_id: interact.inav_id,
-        // 聊天室id
-        channel_id: interact.channel_id,
-        // 直播场次ID
-        switch_id: watchInitData.switch.switch_id,
-        // 语言类型
-        language_type: useRoomBaseState.languages.lang.type,
-        // 用户类型
-        role_name: join_info.role_name,
-        // UA设备
-        ua: window.navigator.userAgent,
-        // 物料种类 【缺少】
-        //file_type:,
-      },
-      ...window.vhallReportForProduct.commonParams,
-      ...{
-        s: watchInitData?.visitor_id || join_info?.third_party_user_id
+    // 链路埋点
+    fullLinkBurningPointReport({
+      reportOptions: reportOptions,
+      useRoomBaseServer: useRoomBaseServer,
+      reportCallback: rId => {
+        setRequestHeaders({
+          'request-id': rId
+        })
       }
-    };
-    // 装饰report函数增加新能力
-    const decorationExtensionReport = (name, execute, source = window.vhallFullLinkBurningPointReport) => {
-      let fn = source[name]
-      source[name] = function (type, options) {
-
-        // options => null {} {report_extra:{}}
-        requestId = randomCode(type);
-
-        // 此处兼容历史上报数据扩展字段缺失问题
-        if (!options) {
-          options = { report_extra: { request_id: requestId } }
-        } else if (!options.report_extra) {
-          options.report_extra = {
-            request_id: requestId
-          }
-        }
-
-        // options => { report_extra: { request_id: 1 } }
-        // 此处兼容老的上报参数，新上报参数，需要 report_extra 为必填项
-        if (!('request_id' in options.report_extra))
-          options.report_extra['request_id'] = requestId;
-        else
-          requestId = options.report_extra.request_id;
-
-        return execute(fn.bind(source, type, options))
-      }
-    }
-    decorationExtensionReport('report', report => {
-      // 设置请求Header 唯一值，用于上报信息关联
-      setRequestHeaders({
-        'request-id': requestId
-      })
-      // 上报开关
-      isReport && report();
-    })
-
-
-    /**
-     * 链路开始上报
-     * @param {Number} eventId 事件ID
-     * @param {Array} relationalEventIds  事件ID对应的结果链路ID
-     * @param {Object} extendOptions 扩展参数
-     */
-    window.vhallReportForProduct.toStartReporting = (eventId, relationalEventIds, extendOptions = {}) => {
-
-      let _randomCode = randomCode(eventId);
-
-      if (typeof relationalEventIds === 'number') relationalEventIds = [relationalEventIds];
-
-      relationalEventIds.forEach(element => {
-        cacheReportCode[element] = _randomCode
-      });
-      window.vhallFullLinkBurningPointReport.report(eventId, {
-        report_extra: {
-          ...{
-            request_id: _randomCode
-          },
-          ...extendOptions
-        }
-      });
-    }
-
-
-    /**
-     * 链路结果上报
-     * @param {Number} resultEventId 事件ID
-     * @param {Object} extendOptions 扩展参数
-     */
-    window.vhallReportForProduct.toResultsReporting = (resultEventId, extendOptions = {}) => {
-
-      window.vhallFullLinkBurningPointReport.report(resultEventId, {
-        report_extra: {
-          ...{
-            request_id: cacheReportCode[resultEventId]
-          },
-          ...extendOptions
-        }
-      });
-    }
-
+    });
   }
+
 
 }
 const version = '__VERSION__';
--- src/utils/report.js
+++ src/utils/report.js
@@ -45,3 +45,161 @@ export function report_interactive_stream_mute(eventData) {
     console.log(err);
   }
 }
+
+/**
+ *
+ * @param {*} options
+ */
+export function fullLinkBurningPointReport(options) {
+
+  // 测试上报开关
+  let isReport = true;
+
+
+  // let isReport = sessionStorage.getItem('isReport');
+  // if (!JSON.parse(isReport)) {
+  //   isReport = confirm("是否开启上报功能");
+  //   sessionStorage.setItem('isReport', isReport)
+  // }
+
+
+  let requestId = 'xxx';
+  let cacheReportCode = {};
+  const useRoomBaseState = options.useRoomBaseServer().state;
+  const { watchInitData } = useRoomBaseState;
+  const { join_info = {}, webinar = {}, interact = {}, sso = {} } = watchInitData;
+
+  // 生成request_id 规则：用户ID + 活动ID + 时间戳 + 事件ID
+  const randomCode = (type = 0) => {
+    let onlyTimeStamp = new Date().getTime();
+    return `${join_info.third_party_user_id}${webinar.id}${onlyTimeStamp}${type}`;
+  }
+
+  window.vhallFullLinkBurningPointReport = new VhallReportForProduct(options.reportOptions);
+
+  // 上报地址
+  window.vhallFullLinkBurningPointReport.BASE_URL = options.reportOptions.env === 'test' ? 'https://t-dc.e.vhall.com/login' : 'https://dc.e.vhall.com/login';
+
+  // 扩展实例后的全局通用上报属性
+  window.vhallFullLinkBurningPointReport.commonParams = {
+    ...{
+      os: 10,
+      // B端账号id
+      business_uid: join_info.third_party_user_id,
+      // 游客ID
+      visitor_id: watchInitData.visitor_id,
+      // 用户唯一id
+      sso_union_id: sso.kick_id,
+      // 用户昵称
+      nickname: encodeURIComponent(join_info.nickname),
+      // 回放ID
+      record_id: webinar.id,
+      // 活动名称(内容名称)
+      webinar_name: webinar.userinfo.nickname,
+      // 是否登录状态
+      is_login: join_info.user_id,
+      // 房间ID
+      room_id: interact.room_id,
+      // 互动房间id
+      inav_id: interact.inav_id,
+      // 聊天室id
+      channel_id: interact.channel_id,
+      // 直播场次ID
+      switch_id: watchInitData.switch.switch_id,
+      // 语言类型
+      language_type: useRoomBaseState.languages.lang.type,
+      // 用户类型
+      role_name: join_info.role_name,
+      // UA设备
+      ua: window.navigator.userAgent,
+      // 物料种类 【缺少】
+      //file_type:,
+    },
+    ...window.vhallReportForProduct.commonParams,
+    ...{
+      s: watchInitData?.visitor_id || join_info?.third_party_user_id
+    }
+  };
+
+  // 装饰report函数增加新能力
+  const decorationExtensionReport = (name, execute, source = window.vhallFullLinkBurningPointReport) => {
+    let fn = source[name]
+    source[name] = function (type, options) {
+
+      // options => null {} {report_extra:{}}
+      requestId = randomCode(type);
+
+      // 此处兼容历史上报数据扩展字段缺失问题
+      if (!options) {
+        options = { report_extra: { request_id: requestId } }
+      } else if (!options.report_extra) {
+        options.report_extra = {
+          request_id: requestId
+        }
+      }
+
+      // options => { report_extra: { request_id: 1 } }
+      // 此处兼容老的上报参数，新上报参数，需要 report_extra 为必填项
+      if (!('request_id' in options.report_extra))
+        options.report_extra['request_id'] = requestId;
+      else
+        requestId = options.report_extra.request_id;
+
+      return execute(fn.bind(source, type, options))
+    }
+  }
+  decorationExtensionReport('report', report => {
+    options.reportCallback(requestId)
+    // 设置请求Header 唯一值，用于上报信息关联
+    // setRequestHeaders({
+    //   'request-id': requestId
+    // })
+    // 上报开关
+    isReport && report();
+  })
+
+
+  /**
+   * 链路开始上报
+   * @param {Number} eventId 事件ID
+   * @param {Array} relationalEventIds  事件ID对应的结果链路ID
+   * @param {Object} extendOptions 扩展参数
+   */
+  window.vhallReportForProduct.toStartReporting = (eventId, relationalEventIds, extendOptions = {}) => {
+
+    let _randomCode = randomCode(eventId);
+
+    if (typeof relationalEventIds === 'number') relationalEventIds = [relationalEventIds];
+
+    relationalEventIds.forEach(element => {
+      cacheReportCode[element] = _randomCode
+    });
+    window.vhallFullLinkBurningPointReport.report(eventId, {
+      report_extra: {
+        ...{
+          request_id: _randomCode
+        },
+        ...extendOptions
+      }
+    });
+  }
+
+
+  /**
+   * 链路结果上报
+   * @param {Number} resultEventId 事件ID
+   * @param {Object} extendOptions 扩展参数
+   */
+  window.vhallReportForProduct.toResultsReporting = (resultEventId, extendOptions = {}) => {
+
+    window.vhallFullLinkBurningPointReport.report(resultEventId, {
+      report_extra: {
+        ...{
+          request_id: cacheReportCode[resultEventId]
+        },
+        ...extendOptions
+      }
+    });
+  }
+
+}
