a7198fe81a594e0a4c952a8ad67ea4f67ef7a955
qiang.zhao <qiang.zhao@vhall.com>
2022-10-14 18:06:20 +0800

    问卷测试包

--- src/sdk/lib/paas-sdk.js
+++ src/sdk/lib/paas-sdk.js
@@ -26,6 +26,7 @@ export default {
   questionnaire: {
     name: 'VhallQuestionnaire',
     url: '//s3.e.vhall.com/common-static/middle/questionnaire/1.0.14/questionnaire_service.js',
+    url: 'https://t-vhallsaas-static.oss-cn-beijing.aliyuncs.com/common-static/middle/questionnaire/1.0.14-test1/questionnaire_service.js',
     defer: true // 新增属性，延迟执行
   }
 };
